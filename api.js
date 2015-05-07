7cc95789f6745ea1e2814ab78745c15968a441c1
Mark Stacy <mbstacy@gmail.com>
2015-05-07 12:04:49 -0500

    Templates for GCM and updated api.js to submit task

--- api.js
+++ api.js
@@ -6,6 +6,8 @@ $(function() {
     logout_url = base_url + "/api-auth/logout/?next=";
     user_task_url = base_url + "/queue/usertasks/.json?page_size=10";
     user_url = base_url + "/user/?format=json";
+    task_url = base_url + "/queue/run/cscq.data.concat.ncrcat/.json"
+    task_data = {"function": "cscq.data.concat.ncrcat","queue": "celery","args":[],"kwargs":{},"tags":[]}
     prevlink=null;nextlink=null;
     set_auth(base_url,login_url);
     $("#aprofile").click(function(){activaTab('profile')})
@@ -20,10 +22,142 @@ $(function() {
                     return ""
                 } 
     });
-    //$('#reset_password').click(function(){$('#pass_form').toggle(!$('#pass_form').is(':visible'));});
-    //$('#user_form').submit(function(){var formData = JSON.parse($("#user_form").serializeArray());console.log(formData);return false;})
+    set_cmip5_form();
+    $('#parameter').change(function(){set_cmip5_domain();});
+    all_model=[];
 });//End of Document Ready
-
+function task_submit(){
+    form_data= $('#task_form').serializeObject()
+    task_data.args = form_data.args 
+    $('#task_result').empty();
+    $.postJSON(task_url,task_data,function(data){
+        $('#task_result').empty();
+        $('#task_result').append("<pre>" + JSON.stringify(data,null, 4) + "</pre>")
+        $('#task_result').urlize();
+        //Reload task history to include the last run
+        load_task_history(user_task_url);
+        }, function(xhr,textStatus,err){
+            $('#task_result').empty();
+            $('#task_result').append("<pre>" + JSON.stringify({"ERROR":textStatus},null, 4) + "</pre>")
+        });
+    return false;
+}
+$.postJSON = function(url, data, callback,fail) {
+    return jQuery.ajax({
+        'type': 'POST',
+        'url': url,
+        'contentType': 'application/json',
+        'data': JSON.stringify(data),
+        'dataType': 'json',
+        'success': callback,
+        'error':fail
+        /*'beforeSend':function(xhr, settings){
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }*/
+    });
+};
+function set_cmip5_form(){
+    //tmpl-cmip5.handlebars  tmpl-option.handlebars
+    $('#home').empty()
+    tmpl_cmip5 =  Handlebars.templates['tmpl-cmip5']
+    $('#home').append(tmpl_cmip5({'csrftoken':getCookie('csrftoken')}))
+    set_cmip5_select("variable",null,"parameter");
+}
+function set_cmip5_domain(){
+    $('#domain').unbind();
+    $('#domain_div').empty()
+    $('#experiment_div').empty()
+    $('#ensemble_div').empty()
+    $('#model_div').empty()
+    tmpl_select =  Handlebars.templates['tmpl-select']
+    $('#domain_div').append(tmpl_select({'id':'domain','label':'Temporal Resolution'}))
+    $('#domain').change(function(){set_cmip5_experiment();});
+    variable = 'domain'
+    query = "{'spec':{'variable':'" + $('#parameter').val() + "'}}"
+    set_cmip5_select(variable,query,variable);
+}
+function set_cmip5_experiment(){
+    $('#experiment').unbind()
+    $('#experiment_div').empty()
+    $('#ensemble_div').empty()
+    $('#model_div').empty()
+    tmpl_select =  Handlebars.templates['tmpl-select']
+    $('#experiment_div').append(tmpl_select({'id':'experiment','label':'Projections'}))
+    $('#experiment').change(function(){set_cmip5_model();});
+    variable = 'experiment'
+    query = "{'spec':{'variable':'" + $('#parameter').val() + "','domain':'" + $('#domain').val() +  "'}}"
+    set_cmip5_select(variable,query,variable);
+}
+function set_cmip5_model(){
+    $('#model').unbind();
+    $('#model_div').empty()
+    $('#ensemble_div').empty()
+    tmpl_select =  Handlebars.templates['tmpl-select']
+    $('#model_div').append(tmpl_select({'id':'model','label':'Model'}))
+    $('#model').change(function(){set_cmip5_ensemble();});
+    variable = 'model'
+    query = "{'spec':{'variable':'" + $('#parameter').val() + "','domain':'" + $('#domain').val() +  "','experiment':'" + $('#experiment').val() + "'}}"
+   // query = query + "','ensemble':'" + $('#ensemble').val()  + "'}}"
+    set_cmip5_select(variable,query,variable);
+}
+function set_cmip5_ensemble(){
+    $('#ensemble').unbind()
+    $('#ensemble_div').empty()
+    tmpl_select =  Handlebars.templates['tmpl-select']
+    $('#ensemble_div').append(tmpl_select({'id':'ensemble','label':'Ensemble'}))
+    $('#ensemble').change(function(){set_result();});
+    variable = 'ensemble'
+    query = "{'spec':{'variable':'" + $('#parameter').val() + "','domain':'" + $('#domain').val() +  "','experiment':'" + $('#experiment').val() 
+    if ($('#model').val()=="all"){
+        query = query + "'}}"
+    }else{
+        query = query + "','model':'" + $('#model').val() + "'}}"
+    }
+    set_cmip5_select(variable,query,variable);
+}
+function set_result(){
+    $('#task_result').empty()
+    url = "http://data.southcentralclimate.org/api/catalog/data/data_portal/cmip5_files/.json?page_size=0&"
+    query = "{'spec':{'variable':'" + $('#parameter').val() + "','domain':'" + $('#domain').val() +  "','experiment':'" + $('#experiment').val()
+    if ($('#model').val()=="all"){
+        query = query + "','ensemble':'" + $('#ensemble').val() +  "'},'$orderby':{'time':1}}"
+    }else{
+        query = query + "','ensemble':'" + $('#ensemble').val() + "','model':'" + $('#model').val() + "'},'$orderby':{'time':1}}"
+    }
+    $.getJSON(url + "query=" + query, function(data){
+        var totalsize = 0;
+        var models =[]
+        $.each(data.results,function(key,item){
+            var temp="";
+            //console.log(item);
+            models.push(item.model);
+            temp = item.size;
+            temp = temp.replace("K",'');
+            totalsize=totalsize + +temp ;   
+        });
+        all_model= $.unique(models)
+        $('#task_result').append("<pre>Total Size = " + totalsize.toString() + "  "  + JSON.stringify(data, null, 4) + "</pre>")
+    });
+}
+function set_cmip5_select(variable,query,selectID){
+    url="http://data.southcentralclimate.org/api/catalog/data/data_portal/cmip5_files/.json?action=distinct&field=" + variable
+    if (query != null){
+        url = url + "&query=" + query
+    }
+    $.getJSON(url,function(data){
+        $('#'+ selectID).append($("<option selected disabled></option>").text("Please Select"));
+        if(variable=="model"){
+            $('#'+ selectID).append($("<option></option>").attr("value","all").text("All Models"));
+            all_model = data.sort();
+        }
+        $.each(data.sort(), function(key, value) {
+            $('#'+ selectID)
+                .append($("<option></option>")
+                .attr("value",value)
+                .text(value));
+        });
+    });
+}
 function submit_user(){
     console.log(user_url)
     $.post( user_url,$('#user_form').serializeObject(),function(data){
@@ -52,7 +186,7 @@ function edit_user(){
 function set_password(){
     pass = $('#pass_form').serializeObject()
     if (pass.password !== pass.password2){
-        alert("Passwords where not identical")
+        alert("Passwords were not identical.")
         return false;
 
     }
